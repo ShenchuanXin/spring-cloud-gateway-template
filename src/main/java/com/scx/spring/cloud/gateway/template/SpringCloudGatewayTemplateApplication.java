e457ec060b51ad1ea4d334b402ca6245bf7c2ddd
shenchuanxin <shenchuanxin@csftgroup.com>
2020-07-17 15:41:16 +0800

    init project

---
+++ src/main/java/com/scx/spring/cloud/gateway/template/SpringCloudGatewayTemplateApplication.java
@@ -0,0 +1,13 @@
+package com.scx.spring.cloud.gateway.template;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringCloudGatewayTemplateApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringCloudGatewayTemplateApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/scx/spring/cloud/gateway/template/SpringCloudGatewayTemplateApplicationTests.java
@@ -0,0 +1,13 @@
+package com.scx.spring.cloud.gateway.template;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringCloudGatewayTemplateApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
